fc57a1677ad273d3dbe2556d4d5cd83e787e3d01
tranphuochiep <tranphuochiep1997@gmail.com>
2021-06-27 18:37:51 +0700

    Update

--- src/main/java/com/example/gateway/GatewayApplication.java
+++ src/main/java/com/example/gateway/GatewayApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 @EnableDiscoveryClient
 public class GatewayApplication {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
